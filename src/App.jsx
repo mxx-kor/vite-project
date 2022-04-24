e77382564734a1eb63934109b427266cd46bfaf8
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-04-24 16:59:01 +0900

    set redux, dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is my first Vite + React App</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
+      hello
     </div>
   )
 }
---
+++ src/redux/reducers/index.jsx
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import { productReducer } from "./ProductReducer";
+
+const reducers = combineReducers({
+    allProducts: productReducer,
+})
+
+export default reducers
