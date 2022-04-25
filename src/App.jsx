14f4cc7e333fd48dd91e668bbf0ba5b49b58097f
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-04-25 18:44:10 +0900

    react router dom 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './containers/Header';
 import './App.css'
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
 
   return (
     <div className="App">
-      hello
+      <Router>
+        <Header />
+          <Routes>
+            <Route path='/' exact element={<ProductListing />} />
+            <Route path='/product/:productId' exact element={<ProductDetail />} />
+            <Route>404 Not Found!</Route>
+          </Routes>
+      </Router>
     </div>
   )
 }
