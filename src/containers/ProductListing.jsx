62deb8d1e73093f2bc3200a7f834a5a96ce9d801
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-04-26 15:48:23 +0900

    connected fake API and dispatched to reducer

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
+import { SetProducts } from "../redux/actions/productActions";
 
 const ProductListing = () => {
     const products = useSelector((state) => state);
-    console.log(products)
+    const dispatch = useDispatch(); 
+
+    const fetchProducts = async () => {
+        const response = await axios.get("https://fakestoreapi.com/products").catch((err) => {
+            console.log("Err", err);
+        });
+        dispatch(SetProducts(response.data));
+    };
+    useEffect(()=>{
+        fetchProducts();
+    }, [])
+
+    console.log("products: ", products)
     return (
         <div className="ui grid container">
             <ProductComponent />
--- src/redux/actions/productActions.jsx
+++ src/redux/actions/productActions.jsx
@@ -2,7 +2,7 @@ import { ActionTypes } from "../constants/action-types";
 export const SetProducts = (products) => {
     return {
         type: ActionTypes.SET_PRODUCTS,
-        payload: products
+        payload: products,
     };
 };
 
