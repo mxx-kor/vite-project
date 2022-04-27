354c05d6c3e11803e6e3d7ea2920d5804981ca44
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-04-27 19:59:21 +0900

    Link로 디테일 페이지 연결 useparams로 id 가져옴

--- src/containers/ProductDetail.jsx
+++ src/containers/ProductDetail.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { SelectedProduct } from "../redux/actions/productActions";
 
 const ProductDetail = () => {
+    const product = useSelector((state) => state.product)
+    const {productId} = useParams();
+    const dispatch = useDispatch();
+    console.log(product)
+
+    const fetchProductDetail = async () => {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`)
+        .catch(err => console.log("Err ", err));
+
+        dispatch(SelectedProduct(response.data));
+    };
+    useEffect(() => {
+        if(productId && productId !== "") fetchProductDetail();
+    }, [productId])
     return (
         <div>
             <h1>ProductDetail</h1>
